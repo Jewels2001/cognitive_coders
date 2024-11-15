9008976b8550aaa289c86044b0683975e87e8f67
John S. Anvik <66496635+jsanvik@users.noreply.github.com>
2024-11-15 12:47:17 -0700

    - Created rough draft for GUI with label selection, file input and a submit button - File picker doesn't work yet

--- gui.py
+++ gui.py
@@ -1,15 +1,33 @@
 import customtkinter
+import widgets
 
 class App(customtkinter.CTk):
+    
     def __init__(self):
         super().__init__()
-        self.geometry("400x150")
+        self.geometry("400x400")
+        self.title("Cognitive Coders")
+        
+        # Default message for label selection menu
+        self.defmessage = 'Select a Label'
+        # Labels for the selection menu
+        self.labels = ['Happy', 'Sad', 'Neutral', 'Angry']
 
-        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
-        self.button.pack(padx=20, pady=20)
-
-    def button_callbck(self):
-        print("HELLO THERE!")
+        # Label Selection Menu
+        self.labels_menu = widgets.SelectionMenu(self, title='Label', options=self.labels, default_message=self.defmessage)
+        self.labels_menu.grid(row=0, column=0, padx=10, pady=(10,0), sticky='nw')
+        
+        # Choose destination file path
+        self.file_picker = widgets.FilePickerButton(self, title='Select File')
+        self.file_picker.grid(row=1, column=0, padx=10, pady=(10,0), sticky='nw')
+        
+        # Submit button
+        self.button = customtkinter.CTkButton(self, text="Generate Data", command=self.printOption)
+        self.button.grid(row=0, column=1, padx=10, pady=20, sticky='nw')
+    
+    # Change this method to do whatever the submit button does - currently prints selected label
+    def printOption(self):
+        print(self.labels_menu.getSelected())
 
 app = App()
 app.mainloop()
---
+++ widgets.py
@@ -0,0 +1,83 @@
+import customtkinter
+
+class SelectionMenu(customtkinter.CTkFrame):
+    def __init__(self, master,  title='Options', options=[], default_message='Please Select an option:'):
+        super().__init__(master)
+
+        self.values = options
+        self.grid_columnconfigure(0, weight=1)
+        
+        # Set the title
+        self.title=title        
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="transparent", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="new")
+        
+        self.options = customtkinter.CTkOptionMenu(self, values=self.values)
+        self.options.set(default_message)
+        self.options.grid(row=1, column=0, padx=10, pady=(10,0), sticky='new')
+        
+    def getSelected(self):
+        return self.options.get()
+
+class FilePickerButton(customtkinter.CTkFrame):
+    def __init__(self, master, title='Select File', filetypes=(('csv files', '*.csv'), ('Excel files', '*.xlsx'), ("all files", "*.*"))):
+        super().__init__(master)
+        
+        self.filetypes = filetypes
+        self.filename = "NO FILE SELECTED"
+        
+        self.button = customtkinter.CTkButton(self, text="Select File", command=self.pick_file())
+        self.button.grid(row=1, column=0, padx=10, pady=(10,0), sticky='new')
+        
+        self.filenamedisplay = customtkinter.CTkLabel(self, text="FileName: " + self.get_filename(), fg_color="transparent")
+        self.filenamedisplay.grid(row=2, column=0, padx=10, pady=(10,0), sticky='new')
+        
+    def pick_file(self):
+        file = customtkinter.filedialog.askopenfilename(filetypes=self.filetypes)
+        if file:
+            self.filename=file
+
+    def get_filename(self):
+        return self.filename
+    
+
+class VerticalRadiobuttonFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master,  title='Radio Buttons', values=[],):
+        super().__init__(master)
+
+        self.values = values
+        self.radiobuttons = []
+        self.grid_columnconfigure(0, weight=1)
+        self.variable = customtkinter.StringVar(value="")
+        
+        # Set the title
+        self.title=title        
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+        
+        for i, value in enumerate(self.values):
+            radiobutton = customtkinter.CTkRadioButton(self, text=value, value=value, variable=self.variable)
+            radiobutton.grid(row=i+1, column=0, padx=10, pady=(10,0), sticky='w')
+            self.radiobuttons.append(radiobutton)
+    def get(self):
+        return self.variable.get()
+            
+class CheckboxFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master,  title='Checkboxes', values=[],):
+        super().__init__(master)
+
+        self.values = values
+        self.checkboxes = []
+        self.grid_columnconfigure(0, weight=1)
+        
+        # Set the title
+        self.title=title        
+        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
+        
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i+1, column=0, padx=10, pady=(10,0), sticky='w')
+            self.checkboxes.append(checkbox)
+    def get(self):
+        return [checkbox.get() for checkbox in self.checkboxes]
