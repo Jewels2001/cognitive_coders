fdaaf9b1281253636d15d2da6e445a303b6ab006
J2ckyo <Jacksimonton@outlook.com>
2024-11-16 14:17:30 -0700

    GUI Layout changes

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ class App(customtkinter.CTk):
         # Sidebar for File selection and Label
 
         self.sidebar_frame2 = customtkinter.CTkFrame(self, width=140, corner_radius=5)
-        self.sidebar_frame2.grid(row=0, column=3, rowspan=4, sticky="nsew", padx=(20, 0), pady=(0, 10))
+        self.sidebar_frame2.grid(row=0, column=3, rowspan=4, sticky="nsew", padx=(20, 0), pady=(0, 0))
         self.sidebar_frame2.grid_rowconfigure(4, weight=1)
 
         # File selection
@@ -72,26 +72,26 @@ class App(customtkinter.CTk):
         self.file_button.grid(row=1, column=3, padx=20, pady=10, sticky='nsew')
 
         # Option Menu
-        self.drop_down_label = customtkinter.CTkLabel(self.sidebar_frame2, text="Select EEG: ", anchor="w")
-        self.drop_down_label.grid(row=3, column=3, padx=20, pady=(40, 0))
+        self.drop_down_label = customtkinter.CTkLabel(self.sidebar_frame2, text="Select EEG: ", fg_color="transparent", corner_radius=6)
+        self.drop_down_label.grid(row=4, column=3, padx=20, pady=(10, 0), sticky='nsew')
         self.drop_down_label_option_menu = customtkinter.CTkOptionMenu(self.sidebar_frame2, values=["Right", "Left"],
                                                                        command=lambda values:
                                                                        function.set_label(self, values))
-        self.drop_down_label_option_menu.grid(row=4, column=3, padx=20, pady=(0, 140))
+        self.drop_down_label_option_menu.grid(row=5, column=3, padx=20, pady=(0, 220))
 
         # Generate Button
         self.generate_button = customtkinter.CTkButton(self.sidebar_frame2, text="Generate EEG",
                                                        command=lambda: function.set_data(self, function.get_label(self)))
-        self.generate_button.grid(row=5, column=3, padx=20, pady=(20, 10))
+        self.generate_button.grid(row=6, column=3, padx=20, pady=(10, 20))
 
         # Download button
         self.Download_button = customtkinter.CTkButton(self.sidebar_frame2, text="Download",
                                                        command=lambda: function.download(self, function.get_label(self), function.get_folder(self), function.get_label(self)), state='disabled')
-        self.Download_button.grid(row=6, column=3, padx=20, pady=(10, 20))
+        self.Download_button.grid(row=3, column=3, padx=20, pady=(10, 10))
 
         # Show Plots button
         self.Show_Plot_button = customtkinter.CTkButton(self.sidebar_frame2, text="Open EEG Plots", command=lambda: function.plot(self, function.get_data(self)))
-        self.Show_Plot_button.grid(row=7, column=3, padx=20)
+        self.Show_Plot_button.grid(row=7, column=3, padx=20, pady=(0, 20))
 
         # Project Description
         self.appearance_mode_optionemenu.set("System")
